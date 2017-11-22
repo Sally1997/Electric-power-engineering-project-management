4f94c1dcb5a31902934277a9c2ae638cf72c101f
Sally1997 <friendwithsummer@163.com>
2017-11-22 18:55:33 +0800

    袁奇中: sessionListner以及servletContextListner，用于统计实时在线人数以及清除失效的session，解决空间

---
+++ RealProject/src/com/holyshit/listener/DeleteSessionListener.java
@@ -0,0 +1,63 @@
+package com.holyshit.listener;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.http.HttpSession;
+
+public class DeleteSessionListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		//新建livingcount存储系统在线人数
+		int num=0;
+		final ServletContext application= arg0.getServletContext();
+		application.setAttribute("livingcount", Integer.toString(num));
+		
+		//新建安全链表
+		final List<HttpSession> list=Collections.synchronizedList(new LinkedList<HttpSession>());
+		application.setAttribute("sessionlist", list);
+		
+		//定时清理失效session  
+		//60s清理一次
+		Timer timer=new Timer();
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				//遍历链表中所有session，如果失效，则删除
+				for(Iterator<HttpSession> iterator=list.iterator();iterator.hasNext();){
+					HttpSession session = iterator.next();
+					long last=session.getLastAccessedTime();
+					//超过5分钟没有操作  失效
+					if(new Date().getTime()-last>5000)
+					{
+						int num=Integer.parseInt((String)application.getAttribute("livingcount"));
+						application.setAttribute("livingcount", Integer.toString(num-1));
+						session.invalidate();
+						iterator.remove();
+					}
+				}
+			}
+		}, 0, 5000);
+		
+		
+	}
+
+}
