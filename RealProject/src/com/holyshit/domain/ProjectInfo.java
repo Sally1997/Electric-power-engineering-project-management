542ced6525e85173666293cfdee5ce3748e1938b
Sally1997 <friendwithsummer@163.com>
2017-12-05 08:26:25 +0800

    丁杰：ProjectInfro

---
+++ RealProject/src/com/holyshit/domain/ProjectInfo.java
@@ -0,0 +1,49 @@
+package com.holyshit.domain;
+
+public class ProjectInfo {
+	private String Pno;
+	private String PName;
+	private String Name;
+	private String PMDuty;
+	private String PType;
+	private String pState;
+	public String getPno() {
+		return Pno;
+	}
+	public void setPno(String pno) {
+		Pno = pno;
+	}
+	public String getPName() {
+		return PName;
+	}
+	public void setPName(String pName) {
+		PName = pName;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getPMDuty() {
+		return PMDuty;
+	}
+	public void setPMDuty(String pMDuty) {
+		PMDuty = pMDuty;
+	}
+	public String getPType() {
+		return PType;
+	}
+	public void setPType(String pType) {
+		PType = pType;
+	}
+	public String getpState() {
+		return pState;
+	}
+	public void setpState(String pState) {
+		this.pState = pState;
+	}
+	public String toString() {
+		return "Project[Pno="+Pno+", PName="+PName+", Name="+Name+"]";
+	}
+}
