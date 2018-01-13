e9ab614f5c0864a9cf4c39391f81691ffc57cb04
Sally1997 <friendwithsummer@163.com>
2018-01-13 10:56:11 +0800

    袁奇中：修复计划阶段人员管理搜索问题

--- RealProject/src/com/holyshit/web/servlet/StaffInfoFindServlet.java
+++ RealProject/src/com/holyshit/web/servlet/StaffInfoFindServlet.java
@@ -23,7 +23,7 @@ public class StaffInfoFindServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
-		
+		request.setCharacterEncoding("UTF-8");
 		String s = request.getParameter("type");
 		
 		//获取pno和staffno
